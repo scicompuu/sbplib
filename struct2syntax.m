d703aa1c42a8001a3d950906941138e8b6df8e05
Jonatan Werpers <jonatan@werpers.com>
2015-09-21 14:50:51 +0200

    Added functions for converting structs to string representations.

---
+++ struct2syntax.m
@@ -0,0 +1,25 @@
+function str = struct2syntax(s)
+    fn = fieldnames(s);
+
+    if length(fn) == 0
+        str = 'struct()';
+        return
+    end
+
+    str = sprintf('struct(');
+
+    for i = 1:length(fn) - 1
+        value = s.(fn{i});
+        str = [str sprintf('''%s'', %s, ',fn{i}, valueString(value))];
+    end
+    value = s.(fn{end});
+    str = [str sprintf('''%s'', %s)',fn{end}, valueString(value))];
+end
+
+function str  = valueString(value)
+    if ischar(value)
+        str = ['''' value ''''];
+    else
+        str = num2str(value);
+    end
+end
