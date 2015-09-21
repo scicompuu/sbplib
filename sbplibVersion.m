0b7c4e2c239e4c8278df34d98503cdd6bcc71224
Jonatan Werpers <jonatan@werpers.com>
2015-09-21 17:40:07 +0200

    Changed to cleaner way of gett the current folder.

--- sbplibVersion.m
+++ sbplibVersion.m
@@ -1,8 +1,7 @@
 % Prints the version and location of the sbplib currently in use.
 function sbplibVersion()
     scriptname  = mfilename('fullpath');
-    I = find(scriptname == '/',1,'last');
-    folder = scriptname(1:I);
+    [folder,~,~] = fileparts(scriptname);
 
     name = 'sbplib';
     ver = '0.0.x';
