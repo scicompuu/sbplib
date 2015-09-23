697987228178fa8804ed11fb5504adc1bc9ae502
Jonatan Werpers <jonatan@werpers.com>
2015-09-23 09:29:53 +0200

    Added functionality for saveing to eps from plotSolutions.

--- +noname/plotSolutions.m
+++ +noname/plotSolutions.m
@@ -2,7 +2,7 @@ function plotSolutions(filename, figename_prefix, plot_type)
     default_arg('figename_prefix',[]);
     default_arg('plot_type','plot')
 
-    save_figures = isempty(figename_prefix);
+    save_figures = ~isempty(figename_prefix);
 
     sf = SolutionFile(filename);
 
@@ -14,7 +14,7 @@ function plotSolutions(filename, figename_prefix, plot_type)
         order   = key.order;
         m       = key.m;
         T       = key.T;
-        repr       = entry.repr;
+        repr    = entry.repr;
         runtime = entry.runtime;
         k       = entry.k;
 
@@ -22,6 +22,13 @@ function plotSolutions(filename, figename_prefix, plot_type)
 
         [update, hand] = discr.setupPlot(plot_type);
         update(repr);
+
+        if save_figures
+            figname = sprintf('%s_%s_o%d_m%d_T%d',figename_prefix,method,order,m,i);
+            fprintf('Saving figure to ''%s''\n',figname);
+            saveeps(hand,figname);
+        end
+
     end
 
 end
