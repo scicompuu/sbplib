1da896aa76296febc642dcda9863b021e4cd8449
Jonatan Werpers <jonatan@werpers.com>
2015-11-11 15:15:26 -0800

    Generalized spdiag. Added function for createing a banded sparse matrix.

--- spdiag.m
+++ spdiag.m
@@ -1,4 +1,4 @@
 function A = spdiag(a,i)
-    n = length(a);
+    n = length(a)-abs(i);
     A = spdiags(a,i,n,n);
 end
---
+++ stripeMatrix.m
@@ -0,0 +1,7 @@
+% Creates a matrix of size n,m with the values of val on the diagonals diag.
+function A = stripeMatrix(val,diags,n,m)
+    default_arg('m',n);
+
+    D = ones(n,1)*val;
+    A = spdiags(D,diags,n,m);
+end
