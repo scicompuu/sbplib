077304d698e0b7e247c3f41fe68861135250fd8e
Jonatan Werpers <jonatan@werpers.com>
2015-12-15 16:38:17 +0100

    I added remaining upwind operators.

--- +sbp/Upwind.m
+++ +sbp/Upwind.m
@@ -11,16 +11,25 @@ classdef Upwind < sbp.OpSet
     methods
         function obj = Upwind(m,h,order)
 
-            if order == 3
-                [H, HI, Dp, Dm, e_1, e_m] = sbp.upwind3_3bp(m,h);
-            elseif order == 4
-                [H, HI, Dp, Dm, e_1, e_m] = sbp.upwind4(m,h);
-            elseif order == 6
-                [H, HI, Dp, Dm, e_1, e_m] = sbp.upwind6(m,h);
-            elseif order == 8
-                [H, HI, Dp, Dm, e_1, e_m] = sbp.upwind8(m,h);
-            else
-                error('Invalid operator order %d.',order);
+            switch order
+                case 2
+                    [H, HI, Dp, Dm, e_1, e_m] = sbp.upwind2(m,h);
+                case 3
+                    [H, HI, Dp, Dm, e_1, e_m] = sbp.upwind3(m,h);
+                case 4
+                    [H, HI, Dp, Dm, e_1, e_m] = sbp.upwind4(m,h);
+                case 5
+                    [H, HI, Dp, Dm, e_1, e_m] = sbp.upwind5(m,h);
+                case 6
+                    [H, HI, Dp, Dm, e_1, e_m] = sbp.upwind6(m,h);
+                case 7
+                    [H, HI, Dp, Dm, e_1, e_m] = sbp.upwind7(m,h);
+                case 8
+                    [H, HI, Dp, Dm, e_1, e_m] = sbp.upwind8(m,h);
+                case 9
+                    [H, HI, Dp, Dm, e_1, e_m] = sbp.upwind9(m,h);
+                otherwise
+                    error('Invalid operator order %d.',order);
             end
 
             obj.h = h;
