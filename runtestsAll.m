fbd03ac56e9156f38377ba6183168dd9dd92b70e
Jonatan Werpers <jonatan@werpers.com>
2016-02-23 13:22:00 +0100

    Added a function to run all tests in a folder including all tests in subpackages.

---
+++ runtestsAll.m
@@ -0,0 +1,22 @@
+function res = runtestsAll()
+    l = dir();
+
+    dirNames = {l([l.isdir]).name};
+
+    packages = {};
+    for i = 1:length(dirNames)
+        if dirNames{i}(1) == '+'
+            packages{end+1} = dirNames{i}(2:end);
+        end
+    end
+
+    rootSuite = matlab.unittest.TestSuite.fromFolder(pwd);
+    packageSuites = {};
+    for i = 1:length(packages)
+        packageSuites{i} = matlab.unittest.TestSuite.fromPackage(packages{i});
+    end
+
+    ts = [rootSuite, packageSuites{:}];
+
+    res = ts.run();
+end
