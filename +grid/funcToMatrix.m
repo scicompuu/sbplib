9b164cc4e119350d274f1364f9d40b6715393aa8
Jonatan Werpers <jonatan@werpers.com>
2016-02-23 10:55:46 +0100

    Updated function name reference.
    --HG--
    branch : feature/grids

--- +grid/funcToMatrix.m
+++ +grid/funcToMatrix.m
@@ -1,5 +1,5 @@
 % Converts a gridfunction to a matrix
 % Takes a grid function and and a structured grid.
 function F = funcToMatrix(g, gf)
-    reshapeKronVector(gf, g.size());
+    reshapeRowMaj(gf, g.size());
 end
