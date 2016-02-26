d038bf14f7623be314e563766c75da698068ac2f
Jonatan Werpers <jonatan@werpers.com>
2016-02-26 16:06:52 +0100

    Added a comment regardning structured grids.
    --HG--
    branch : feature/grids

--- +grid/Structured.m
+++ +grid/Structured.m
@@ -1,7 +1,7 @@
 classdef Structured < grid.Grid
     methods (Abstract)
         % Returns the size of the grid in each dimension m = [mx my mz ...]
-        m = size(obj);
+        m = size(obj); % Is this a good idea? Isn't immersed a structured grid?
         h = scaling(obj);
     end
 end
