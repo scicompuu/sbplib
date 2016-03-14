4ddcb62ba7cf261fcdb27856b0d2c283e04b9025
Jonatan Werpers <jonatan@werpers.com>
2016-03-14 13:52:37 +0100

    Added function to set alla font sizes of a figure. Added default dpi to savepng.

--- savepng.m
+++ savepng.m
@@ -1,4 +1,5 @@
 function savepng(h, filename, dpi)
+    default_arg('dpi', 300)
     print(h,filename,'-dpng',sprintf('-r%d',dpi));
     % Let print size in inches as input parameter
     % Smaller boundingbox
