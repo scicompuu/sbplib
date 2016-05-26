2e96419dfc81caffc64d2e30e2e6aa367985c999
Jonatan Werpers <jonatan@werpers.com>
2016-05-26 12:52:59 +0200

    time: Added ability to avarage of several runs.

--- time.m
+++ time.m
@@ -1,10 +1,34 @@
-function t = time(f)
+function t_out = time(f, n)
+    default_arg('n',1);
+
+    if n == 1
+        t = timeOnce(f);
+    else
+        t = timeAvg(f, n);
+    end
+
+    if nargout == 1
+        t_out = t;
+    else
+        fprintf('Elapsed time is %.6f seconds.\n', t)
+    end
+end
+
+function t = timeOnce(f)
     s = tic();
 
     f();
 
-    if nargout == 1
-        t = toc(s);
-    else
-        toc(s);
+    t = toc(s);
+end
+
+
+function t = timeAvg(f, n)
+    s = tic();
+
+    for i = 1:n
+        f();
+    end
+
+    t = toc(s)/n;
 end
