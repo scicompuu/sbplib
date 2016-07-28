0f5e102ac6b3a77330392bc1374503c2f2613453
Jonatan Werpers <jonatan@werpers.com>
2016-07-28 11:45:02 +0200

    grid: Fixed returns of funcToXXX
    --HG--
    branch : feature/beams

--- +grid/funcToPlotMatrix.m
+++ +grid/funcToPlotMatrix.m
@@ -1,5 +1,5 @@
 % Converts a gridfunction to a plot matrix
 % Takes a grid function and and a structured grid.
 function F = funcToPlotMatrix(g, gf)
-    reshapeToPlotMatrix(gf, g.size());
+    F = reshapeToPlotMatrix(gf, g.size());
 end
