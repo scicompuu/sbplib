2867853af35d80a66a74fa2c017c5c9388339f8c
Martin Almquist <martin.almquist@it.uu.se>
2016-09-12 14:10:40 +0200

    plainTextTable working.
    --HG--
    branch : feature/textTable

--- textTable.m
+++ textTable.m
@@ -1,67 +1,60 @@
 
-% data -- cell array of strings for each cell
-function textTable(data,divCol, divColWeight, divRow, divRowWeight)
+% data           -- cell array of numbers
+% leftColstrings -- cell array of strings, for left column
+% topRowStrings  -- cell array of strings, for top row
+% dataFormat     -- (optional) format specifier, e.g. '%.2f' 
+function textTable(data, leftColStrings, topRowStrings, dataFormat)
+
+    default_arg('dataFormat','%.2f')
+    
+    nRows = length(leftColStrings);
+    nCols = length(topRowStrings);
+    [m,n] = size(data);
+    
+    if(m ~= nRows || n ~=nCols)
+        error('Data dimensions must match labels');
+    end
 
     % Find column widths
-    colWidth = {};
-    for i = 1:size(data, 2)
-        for j = 1:size(data, 1)
-            c(j) = length(data{j,i});
+    headerLength = 0;
+    for i = 1:nCols
+        headerLength = max(headerLength, length(topRowStrings{i} ));
+    end
+ 
+    dataLength = 0;
+    for i = 1:nRows
+        for j = 1:nCols
+            temp = length(sprintf(dataFormat, data{i,j}));
+            dataLength = max(dataLength, temp);
         end
-        colWidth{i} = max(c);
     end
+    dataLength = length(sprintf(dataFormat, data{1,1}));
+    
+    colWidth = max(headerLength,dataLength);
 
-    error('not done')
-
-    eW = 0;
-    qW = 0;
-    for i = 1:length(orders)
-        log_e{i} = log10(e{i});
-        eW = max(eW, findFieldWidth('%.2f',log_e{i}));
-        qW = max(qW, findFieldWidth('%.2f',q{i}));
-    end
-
-    mW = findFieldWidth('%d',m);
-    orderHeaderWidth = eW + qW + 1;
-
-    fprintf('method: %s\nT: %d\n',methodName, T);
-
-    % Print order headers
-    fprintf(' %*s |',mW,'')
-    for i = 1:length(orders)
-        fprintf(' %-*s |', orderHeaderWidth, sprintf('Order %d', orders{i}));
+    % Print headers
+    fprintf(' %*s |',colWidth,'')
+    for i = 1:nCols
+        fprintf(' %-*s |', colWidth, topRowStrings{i});
     end
     fprintf('\n');
 
-
-    % Print eq headers
-    fprintf(' %*s |',mW,'m');
-    for i = 1:length(orders)
-        fprintf(' %*s %*s |', eW, 'e', qW, 'q');
-    end
-    fprintf('\n');
-
-
-    % Print devider
-    m_dev = repmat('-',1,mW);
-    column_dev = repmat('-',1,orderHeaderWidth);
+    % Print divider
+    m_dev = repmat('-',1,colWidth);
+    column_dev = repmat('-',1,colWidth);
     fprintf('-%s-+',m_dev);
-    for i = 1:length(orders)
+    for i = 1:nCols
         fprintf('-%s-+', column_dev);
     end
     fprintf('\n');
-
-
+    
 
     % Print each row
-    for i = 1:length(m)
-        fprintf(' %*d |',mW,m(i));
-        for j = 1:length(orders)
-            if i == 1
-                fprintf(' %*.2f %*s |', eW, log_e{j}(i), qW, '');
-            else
-                fprintf(' %*.2f %*.2f |', eW, log_e{j}(i), qW, q{j}(i-1));
-            end
+    dataFormat = ['%*' dataFormat(2:end)];
+    for i = 1:nRows
+        fprintf(' %*s |',colWidth,leftColStrings{i});
+        for j = 1:nCols
+            fprintf([' ' dataFormat ' |'], colWidth, data{i,j});
         end
         fprintf('\n');
     end
