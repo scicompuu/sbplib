4cc6df3f841c241d3e8625b77f7a9ab99aa5a014
Jonatan Werpers <jonatan@werpers.com>
2017-03-19 13:39:49 +0100

    Fix bug in spyh() when the matix has no non-zero elements.

--- spyh.m
+++ spyh.m
@@ -3,10 +3,14 @@ function h = spyh(A)
     [n,m] = size(A);
     [I,J] = find(A);
 
-    h = plot(J,I);
-    h.LineStyle = 'none';
-    h.Marker = '.';
-    h.MarkerSize = 14;
+    if ~isempty(J)
+        h = plot(J,I);
+        h.LineStyle = 'none';
+        h.Marker = '.';
+        h.MarkerSize = 14;
+    else
+        h = [];
+    end
 
     a = gca;
     xlim([0 m+1]);
