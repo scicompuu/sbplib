848e008dab32a4d5f412286b623c9a3c48ea35a0
Jonatan Werpers <jonatan@werpers.com>
2017-08-02 08:48:02 +0200

    Remove debug printing
    --HG--
    branch : feature/sublassable_cellarray

--- callConstructor.m
+++ callConstructor.m
@@ -1,7 +1,6 @@
 % Calls the constructor of an object.
 % Might be usefull to call the constructor of a subclass object in the superclass
 function obj = callConstructor(subclassObj, varargin)
-    varargin
     fun = str2func(class(subclassObj));
     obj = fun(varargin{:});
 end
