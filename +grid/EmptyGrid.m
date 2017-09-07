557029422b86c88d8d84ff39fa9e84b0be303b89
Jonatan Werpers <jonatan@werpers.com>
2017-09-07 09:16:12 +0200

    Add an Empty grid class
    The need turned up for the flexural code when we may or may not have a grid for the open water and want to plot that solution.
    In case there is no open water we need an empty grid to plot the empty gridfunction against to avoid errors.
    
    --HG--
    branch : feature/grids

---
+++ +grid/EmptyGrid.m
@@ -0,0 +1,45 @@
+classdef EmptyGrid < grid.Grid
+    properties
+        dim
+    end
+
+    methods
+        function obj = EmptyGrid(D)
+            obj.dim = D;
+        end
+        % n returns the number of points in the grid
+        function o = N(obj)
+            o = 0;
+        end
+
+        % d returns the spatial dimension of the grid
+        function o = D(obj)
+            o = obj.dim;
+        end
+
+        % points returns a n x d matrix containing the coordinates for all points.
+        function X = points(obj)
+            X = sparse(0,obj.dim);
+        end
+
+        % Restricts the grid function gf on obj to the subgrid g.
+        function gf = restrictFunc(obj, gf, g)
+            error('Restrict does not make sense for an empty grid')
+        end
+
+        % Projects the grid function gf on obj to the grid g.
+        function gf = projectFunc(obj, gf, g)
+            error('Project does not make sense for an empty grid')
+        end
+
+        % Return the grid.boundaryIdentifiers of all boundaries in a cell array.
+        function bs = getBoundaryNames(obj)
+            bs = {};
+        end
+
+        % Return coordinates for the given boundary
+        function b = getBoundary(obj, name)
+            b = sparse(0,obj.dim-1);
+        end
+    end
+end
