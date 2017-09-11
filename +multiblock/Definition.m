c2845ba86f6efef1b1629d05b98e3f6fbd64d5c4
Jonatan Werpers <jonatan@werpers.com>
2017-09-11 13:57:04 +0200

    Remove abstract methods getGridSizes from multiblock.Definition
    --HG--
    branch : feature/better_multiblock_defs

--- +multiblock/Definition.m
+++ +multiblock/Definition.m
@@ -7,9 +7,5 @@ classdef Definition
         % label is the type of label used for plotting,
         % default is block name, 'id' show the index for each block.
         show(obj, label, gridLines, varargin)
-
-        % Returns the grid size of each block in a cell array
-        % The input parameters are determined by the subclass
-        ms = getGridSizes(obj, varargin)
     end
 end
