087c5f8703b8005e4dd11733fb393d22e8b86608
Jonatan Werpers <jonatan@werpers.com>
2018-07-27 09:49:24 -0700

    Add type assertions, fix bug, edit documentation and formatting
    --HG--
    branch : feature/poroelastic

--- +multiblock/evalOn.m
+++ +multiblock/evalOn.m
@@ -1,28 +1,32 @@
-% Useful for evaulating forcing functions with different functional expressions for each block
-% f: cell array of function handles fi
-% f_i = f_i(x1,y,...,t)
-% t: time point. If not specified, it is assumed that the functions take only spatial arguments.
+% Evaluate different function handle for each block in a multiblock.Grid
+% Function handles may optionaly take a time argument
+% f -- cell array of function handles
+%       f{i} = f_i(x,y,...,t)
+% t -- optional time point. If not specified, it is assumed that the functions take only spatial arguments.
 function gf = evalOn(g, f, t)
-    default_arg('t',[]);
+    assertType(g, 'multiblock.Grid');
+    assertType(f, 'cell');
+
+    default_arg('t', []);
 
     grids = g.grids;
     nBlocks = length(grids);
-    gf = cell(nBlocks,1);
+    gf = cell(nBlocks, 1);
 
     if isempty(t)
         for i = 1:nBlocks
-            grid.evalOn(grids{i}, f{i} );
+            gf{i} = grid.evalOn(grids{i}, f{i});
         end
     else
         dim = nargin(f{1}) - 1;
         for i = 1:nBlocks
             switch dim
                 case 1
-                    gf{i} = grid.evalOn(grids{i}, @(x)f{i}(x,t) );
+                    gf{i} = grid.evalOn(grids{i}, @(x)f{i}(x,t));
                 case 2
-                    gf{i} = grid.evalOn(grids{i}, @(x,y)f{i}(x,y,t) );
+                    gf{i} = grid.evalOn(grids{i}, @(x,y)f{i}(x,y,t));
                 case 3
-                    gf{i} = grid.evalOn(grids{i}, @(x,y,z)f{i}(x,y,z,t) );
+                    gf{i} = grid.evalOn(grids{i}, @(x,y,z)f{i}(x,y,z,t));
             end
         end
     end
