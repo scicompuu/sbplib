a622e0c4513c0da42d520b9a2347716236d676e0
Jonatan Werpers <jonatan@werpers.com>
2018-07-26 17:51:48 -0700

    Bort med martins hemska whitespace
    --HG--
    branch : feature/poroelastic

--- spdiagsVariablePeriodic.m
+++ spdiagsVariablePeriodic.m
@@ -2,17 +2,17 @@ function A = spdiagsVariablePeriodic(vals,diags)
     % Creates an m x m periodic discretization matrix.
     % vals - m x ndiags matrix of values
     % diags - 1 x ndiags vector of the 'center diagonals' that vals end up on
-    % vals that are not on main diagonal are going to spill over to 
+    % vals that are not on main diagonal are going to spill over to
     % off-diagonal corners.
 
     default_arg('diags',0);
 
-    [m, ~] = size(vals); 
+    [m, ~] = size(vals);
 
     A = sparse(m,m);
 
     for i = 1:length(diags)
-        
+
         d = diags(i);
         a = vals(:,i);
 
@@ -21,7 +21,7 @@ function A = spdiagsVariablePeriodic(vals,diags)
             a_bulk = a(1+abs(d):end);
             a_corner = a(1:1+abs(d)-1);
             corner_diag = m-abs(d);
-            A = A + spdiagVariable(a_bulk, d); 
+            A = A + spdiagVariable(a_bulk, d);
             A = A + spdiagVariable(a_corner, corner_diag);
 
         % Super-diagonals
@@ -29,7 +29,7 @@ function A = spdiagsVariablePeriodic(vals,diags)
             a_bulk = a(1:end-d);
             a_corner = a(end-d+1:end);
             corner_diag = -m + d;
-            A = A + spdiagVariable(a_bulk, d); 
+            A = A + spdiagVariable(a_bulk, d);
             A = A + spdiagVariable(a_corner, corner_diag);
 
         % Main diagonal
