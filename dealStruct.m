e882cfd6c50f9a4e0c22e5bef7d8ab5cbd2672c4
Jonatan Werpers <jonatan@werpers.com>
2018-11-22 07:26:45 +0100

    Handle default arguments better in dealStruct()

--- dealStruct.m
+++ dealStruct.m
@@ -1,9 +1,18 @@
 function varargout = dealStruct(s, fields)
-    default_arg('fields', fieldnames(s));
+    default_arg('fields', []);
 
-    assert(nargout == length(fields), 'Number of output arguements must match the number of fields');
+    if isempty(fields)
+        out = dealFields(s, fieldnames(s));
+        varargout = out(1:nargout);
+    else
+        assert(nargout == length(fields), 'Number of output arguements must match the number of fieldnames provided');
+        varargout = dealFields(s, fields);
+    end
+end
 
+function out = dealFields(s, fields)
+    out = cell(1, length(fields));
     for i = 1:length(fields)
-        varargout{i} = s.(fields{i});
+        out{i} = s.(fields{i});
     end
 end
