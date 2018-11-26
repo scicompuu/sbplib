aaf5a7b004dae708792b21b6ae60b815e927a933
Jonatan Werpers <jonatan@werpers.com>
2018-11-26 10:00:05 +0100

    Clean up default_field
    --HG--
    branch : feature/utux2D

--- default_field.m
+++ default_field.m
@@ -1,9 +1,6 @@
 function default_field(s, f, val)
-    if isfield(s,f)
-    	field = getfield(s, f);
-    	if ~isempty(field)
-        	return
-        end
+    if isfield(s,f) && ~isempty(s.(f))
+        return
     end
     s.(f) = val;
     assignin('caller', inputname(1),s);
