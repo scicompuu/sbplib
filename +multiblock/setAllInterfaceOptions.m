7d39f0fa6b03a761e6cb2476dfcaca95509292a0
Martin Almquist <malmquist@stanford.edu>
2018-12-03 12:02:27 -0800

    Make multiblock.setAllInterfaceOptions set opts only for those entries where grid.connections indicates interface couplings.
    --HG--
    branch : feature/utux2D

--- +multiblock/setAllInterfaceOptions.m
+++ +multiblock/setAllInterfaceOptions.m
@@ -1,11 +1,15 @@
 % g: multiblock grid
 % opts: struct of options
-% Returns g.nBlocks x g.nBlocks cell array with all elements set to opts.
+% Returns g.nBlocks x g.nBlocks cell array with all interface opts set to opts.
 function optsCell = setAllInterfaceOptions(g, opts)
 
 	optsCell = cell(g.nBlocks(), g.nBlocks());
-	for i = 1:g.nBlocks()^2
-		optsCell{i} = opts;
+	for i = 1:g.nBlocks()
+		for j = 1:g.nBlocks
+			if ~isempty(g.connections{i,j})
+				optsCell{i,j} = opts;
+			end
+		end
 	end
 
 end
