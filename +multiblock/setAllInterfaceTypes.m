e37ffbd2b77d4aae10bc2531187b67d58fc38140
Jonatan Werpers <jonatan@werpers.com>
2018-12-04 12:37:44 +0100

    Change from opts to type in multiblock.setAllInterfaceTypes
    --HG--
    branch : feature/utux2D

--- +multiblock/setAllInterfaceTypes.m
+++ +multiblock/setAllInterfaceTypes.m
@@ -1,15 +1,13 @@
-% g: multiblock grid
-% opts: struct of options
-% Returns g.nBlocks x g.nBlocks cell array with all interface opts set to opts.
-function optsCell = setAllInterfaceTypes(g, opts)
-
-	optsCell = cell(g.nBlocks(), g.nBlocks());
+% Create interface configuration with a single type for all interfaces
+%    g -- multiblock grid
+% type -- type for all interfaces
+function intfTypes = setAllInterfaceTypes(g, type)
+	intfTypes = cell(g.nBlocks(), g.nBlocks());
 	for i = 1:g.nBlocks()
-		for j = 1:g.nBlocks
+		for j = 1:g.nBlocks()
 			if ~isempty(g.connections{i,j})
-				optsCell{i,j} = opts;
+				intfTypes{i,j} = type;
 			end
 		end
 	end
-
 end
