66ffd90c1ef6a4a9140469395068f94626cc4387
Jonatan Werpers <jonatan@werpers.com>
2019-01-16 16:50:21 +0100

    Remove misplaced error functions from +scheme

--- +scheme/errorMax.m
+++
@@ -1,3 +0,0 @@
-function e = errorMax(~, v1, v2)
-    e = max(abs(v1-v2));
-end
--- +scheme/errorSbp.m
+++
@@ -1,6 +0,0 @@
-function e = errorSbp(discr, v1, v2)
-    % If v1 and v2 are more complex types, something like grid functions... Then we may use .getVectorFrom here!
-    H = discr.H;
-    err = v2 - v1;
-    e = sqrt(err'*H*err);
-end
