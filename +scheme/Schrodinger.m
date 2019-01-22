49f36e02425f58ac2c4a3689b51f6b95bb439c3b
Jonatan Werpers <jonatan@werpers.com>
2019-01-22 16:50:05 +0100

    Remove static method `interface_coupling` that shouldn't have existed in the first place

--- +scheme/Schrodinger.m
+++ +scheme/Schrodinger.m
@@ -128,14 +128,4 @@ classdef Schrodinger < scheme.Scheme
         end
 
     end
-
-    methods(Static)
-        % Calculates the matrcis need for the inteface coupling between boundary bound_u of scheme schm_u
-        % and bound_v of scheme schm_v.
-        %   [uu, uv, vv, vu] = inteface_couplong(A,'r',B,'l')
-        function [uu, uv, vv, vu] = interface_coupling(schm_u,bound_u,schm_v,bound_v)
-            [uu,uv] = schm_u.interface(bound_u,schm_v,bound_v);
-            [vv,vu] = schm_v.interface(bound_v,schm_u,bound_u);
-        end
-    end
 end
